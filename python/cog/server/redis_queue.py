4fab38e2a786f92eb90b2fdbe810d9903d9c329e
yekta <ngyekta@gmail.com>
2023-02-04 04:24:22 +0300

    Region name

--- python/cog/server/redis_queue.py
+++ python/cog/server/redis_queue.py
@@ -504,6 +504,7 @@ if __name__ == "__main__":
     parser.add_argument("--s3-secret-key")
     parser.add_argument("--s3-endpoint-url")
     parser.add_argument("--s3-bucket")
+    parser.add_argument("--s3-region")
     parser.add_argument("--consumer-id")
     parser.add_argument("--model-id")
     parser.add_argument("--predict-timeout", type=int)
@@ -530,6 +531,7 @@ if __name__ == "__main__":
         # Configure boto3 client
         s3: ServiceResource = boto3.resource(
             "s3",
+            region_name=args.s3_region,
             endpoint_url=args.s3_endpoint_url,
             aws_access_key_id=args.s3_access_key,
             aws_secret_access_key=args.s3_secret_key,
