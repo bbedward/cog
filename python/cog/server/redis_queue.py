1339e0b3f599ad2fb046954bcbb068f8123e8000
yekta <ngyekta@gmail.com>
2023-02-07 19:13:27 +0300

    Remove the type log

--- python/cog/server/redis_queue.py
+++ python/cog/server/redis_queue.py
@@ -519,7 +519,6 @@ class RedisQueueWorker:
 
         # Load stuff in here or it doesn't work in threadpool
         for uo in uploadObjects:
-            print(f"Type of uo.pil_image: {type(uo.pil_image)}")
             uo.pil_image.load()
         print(f"Loaded all images in: {round((time.time() - start) *1000)} ms")
 
